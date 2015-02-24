1aa0c84728d09d7d84aacbc5125f0700fb235fc7
Tyler Johnson <tyler@vintyge.com>
2015-02-23 19:30:09 -0700

    upgraded for mustache 2.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,26 +2,61 @@ module.exports = function(grunt) {
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		clean: [ "dist/*.js" ],
+		browserify: {
+			test: {
+				src: "test/*.js",
+				dest: "dist/temple-backbone.test.js",
+				options: {
+					browserifyOptions: { debug: true }
+				}
+			}
+		},
 		wrap2000: {
 			dist: {
 				src: 'lib/backbone.js',
-				dest: 'dist/temple.backbone.js'
+				dest: 'dist/temple-backbone.js',
+				options: {
+					header: "/*\n * Temple Backbone\n * (c) 2014-2015 Beneath the Ink, Inc.\n * MIT License\n * Version <%= pkg.version %>\n */\n"
+				}
 			},
-			options: {
-				header: "/*\n * Temple Backbone\n * (c) 2014 Beneath the Ink, Inc.\n * MIT License\n * Version <%= pkg.version %>\n */\n"
+			test: {
+				src: 'dist/temple-backbone.test.js',
+				dest: 'dist/temple-backbone.test.js',
+				options: {
+					header: "/* Temple Mustache Tests / (c) 2014-2015 Beneath the Ink, Inc. / MIT License / Version <%= pkg.version %> */"
+				}
 			}
 		},
 		uglify: {
 			dist: {
-				src: "dist/temple.backbone.js",
-				dest: "dist/temple.backbone.min.js"
+				src: "dist/temple-backbone.js",
+				dest: "dist/temple-backbone.min.js"
+			}
+		},
+		watch: {
+			test: {
+				files: [ "lib/**/*.js", "test/*.js" ],
+				tasks: [ 'test' ],
+				options: { spawn: false }
 			}
 		}
 	});
 
+	grunt.loadNpmTasks('grunt-browserify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-wrap2000');
 
-	grunt.registerTask('default', [ 'wrap2000', 'uglify' ]);
+	grunt.registerTask('build-dev', [ 'wrap2000:dist' ]);
+	grunt.registerTask('build-test', [ 'browserify:test', 'wrap2000:test' ]);
+	grunt.registerTask('build-dist', [ 'wrap2000:dist', 'uglify:dist' ]);
+
+	grunt.registerTask('dev', [ 'clean', 'build-dev' ]);
+	grunt.registerTask('test', [ 'clean', 'build-test' ]);
+	grunt.registerTask('dist', [ 'clean', 'build-dist' ]);
+
+	grunt.registerTask('default', [ 'dist' ]);
 
 }
--- test/backbone.js
+++ test/backbone.js
@@ -1,3 +1,10 @@
+var Backbone = require("backbone"),
+	Mustache = require("temple-mustache"),
+	expect = require("./utils/expect");
+
+// load & register the plugin
+require("../");
+
 describe("Backbone", function() {
 	var tpl, doc;
 
@@ -16,8 +23,8 @@ describe("Backbone", function() {
 		expect(doc.childNodes.length).to.equal(0);
 	});
 
-	function render(template, scope) {
-		tpl = new Mustache(template || "<span></span>", scope);
+	function render(template, data) {
+		tpl = Mustache.render(template || "<span></span>", data);
 		tpl.use("backbone");
 		tpl.paint(doc);
 		return tpl;
@@ -33,105 +40,65 @@ describe("Backbone", function() {
 
 	describe("Model Handler", function() {
 		it("gets from backbone model", function() {
-			render();
 			var model = new Backbone.Model({ foo: "bar" });
-			tpl.set("model", model);
+			render(null, { model: model });
 
 			expect(tpl.get("model.foo")).to.equal("bar");
 		});
 
-		it("sets to backbone model", function() {
-			render();
+		it("gets are reactive on tracked models", function() {
 			var model = new Backbone.Model({ foo: "bar" });
-			tpl.set("model", model);
+			Mustache.Backbone.trackModel(model);
+			render(null, { model: model });
+			var seen = 0;
 
-			tpl.set("model.foo", "Hello World");
-			expect(model.get("foo")).to.equal("Hello World");
-		});
+			tpl.autorun(function() {
+				if (seen === 0) expect(tpl.get("model.baz")).to.be.undefined;
+				if (seen === 1) expect(tpl.get("model.baz")).to.equal("buz");
+				seen++;
+			});
 
-		it("unsets from backbone model", function() {
-			render();
-			var model = new Backbone.Model({ foo: "bar" });
-			tpl.set("model", model);
-
-			tpl.unset("model.foo");
-			expect(model.get("foo")).to.be.undefined;
-		});
-
-		it("returns all properties in model", function() {
-			render();
-			var model = new Backbone.Model({ foo: "bar", baz: "buz" });
-			tpl.set("model", model);
-
-			expect(tpl.keys("model")).to.deep.equal([ "foo", "baz" ]);
-		});
-
-		it("merges plain objects with model", function() {
-			render();
-			var model = new Backbone.Model({ foo: "bar" });
-			tpl.set("model", model);
-
-			tpl.set("model", { baz: "buz" });
-			expect(model.toJSON()).to.deep.equal({ foo: "bar", baz: "buz" });
-		});
-
-		it("notifies temple model of changes to backbone model", function() {
-			render();
-			var model = new Backbone.Model({ foo: "bar" });
-			tpl.set("model", model);
-
-			expect(tpl.get("model.baz")).to.be.undefined;
 			model.set("baz", "buz");
-			expect(tpl.get("model.baz")).to.equal("buz");
+			Mustache.Deps.flush();
+			expect(seen).to.equal(2);
 		});
 	});
 
 	describe("Collection Handler", function() {
 		it("gets model from collection by index", function() {
-			render();
 			var col = new Backbone.Collection([ { foo: "bar" } ]);
-			tpl.set("col", col);
+			render(null, { col: col });
 
 			expect(tpl.get("col.0")).to.be.instanceof(Backbone.Model);
 			expect(tpl.get("col.0").toJSON()).to.deep.equal({ foo: "bar" });
 		});
 
 		it("gets model from collection by id", function() {
-			render();
 			var col = new Backbone.Collection([ { id: "foo" } ]);
-			tpl.set("col", col);
+			render(null, { col: col });
 
 			expect(tpl.get("col.foo")).to.be.instanceof(Backbone.Model);
 			expect(tpl.get("col.foo").toJSON()).to.deep.equal({ id: "foo" });
 		});
 
-		it("sets on model by id", function() {
-			render();
-			var col = new Backbone.Collection([ { id: "foo" } ]);
-			tpl.set("col", col);
-
-			tpl.set("col.foo", { bar: "baz" });
-			expect(col.get("foo").toJSON()).to.deep.equal({ id: "foo", bar: "baz" });
-		});
-
-		it("sets on model by index", function() {
-			render();
-			var col = new Backbone.Collection([ { id: "foo" } ]);
-			tpl.set("col", col);
-
-			tpl.set("col.0", { bar: "baz" });
-			expect(col.get("foo").toJSON()).to.deep.equal({ id: "foo", bar: "baz" });
-		});
-
-		it("notifies temple model of changes to backbone collection", function() {
-			render();
+		it("gets are reactive on tracked collections", function() {
 			var col = new Backbone.Collection();
-			tpl.set("col", col);
+			Mustache.Backbone.trackCollection(col);
+			render(null, { col: col });var seen = 0;
 
-			expect(tpl.get("col.foo")).to.be.undefined;
+			tpl.autorun(function() {
+				if (seen === 0) expect(tpl.get("col.foo")).to.be.undefined;
+				if (seen === 1) {
+					expect(tpl.get("col.foo")).to.be.instanceof(Backbone.Model);
+					expect(tpl.get("col.foo").toJSON()).to.deep.equal({ id: "foo" });
+				}
+
+				seen++;
+			});
+
 			col.add({ id: "foo" });
-			expect(tpl.get("col.foo")).to.be.instanceof(Backbone.Model);
-			expect(tpl.get("col.foo").toJSON()).to.deep.equal({ id: "foo" });
+			Mustache.Deps.flush();
+			expect(seen).to.equal(2);
 		});
 	});
 });
